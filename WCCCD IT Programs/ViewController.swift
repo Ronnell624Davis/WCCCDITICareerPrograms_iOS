725dccd05255a0362d09fab34228b860c33d4387
Ronnell624Davis <nellynel624@gmail.com>
2017-09-12 18:23:32 -0400

    Initial Commit

---
+++ WCCCD IT Programs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WCCCD IT Programs
+//
+//  Created by Ronnell Davis on 9/12/17.
+//  Copyright © 2017 Wayne County Community District College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
